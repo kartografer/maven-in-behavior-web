21df019c507cb818b3e6dbf9cc5bfb6f4c674027
Joel Guilarte <jguilarte01@gmail.com>
2022-06-11 09:28:30 -0400

    feat(firebase): added firebase integration into project to release to firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'maveninbehavior-6c5a6',
+    appId: '1:800275068901:web:db1ca841443ae97e26610c',
+    storageBucket: 'maveninbehavior-6c5a6.appspot.com',
+    apiKey: 'AIzaSyAirsIMqd4Hqk32QF36Z9RDeL-r3RecUKI',
+    authDomain: 'maveninbehavior-6c5a6.firebaseapp.com',
+    messagingSenderId: '800275068901',
+    measurementId: 'G-2BDQQXLX4K',
+  },
   production: false,
 }
 
